123f9ee913899c2febabd133407d787cc30dda16
Matthew Dreyer <team@unit6websites.com>
2017-01-18 16:09:38 +0000

    Updated system/satis.json
     - Added unit6/form.
     - Added unit6/jwt.

--- system/copy.sh
+++ system/copy.sh
@@ -3,9 +3,13 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(dirname "$DIR")"
 PUBLIC="${DIR}/public"
+GIT=$(which git)
 
 cp $PUBLIC/index.html $ROOT_DIR/index.html
 cp $PUBLIC/packages.json $ROOT_DIR/packages.json
 rm $ROOT_DIR/include/*.json
 rmdir $ROOT_DIR/include
 cp -r $PUBLIC/include $ROOT_DIR/include
+
+# update all
+$GIT add $ROOT_DIR
